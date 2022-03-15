b2f1139e0ed9cdea3f66e23f414ce920e08225b7
Thaysa <thay.fernnanda@gmail.com>
2022-03-15 09:53:48 -0300

    adiciona requisito 7

--- src/objPlayground.js
+++ src/objPlayground.js
@@ -36,10 +36,15 @@
 const calculator = (number1, number2) => ({
     sum: number1 + number2,
     mult: number1 * number2,
-    div: number1 / number2,
+    div: Math.floor(number1 / number2),
     sub: number1 - number2,
 });
 console.log(calculator(2, 3));
-const arrayGenerator = (type, object) => { };
+
+const arrayGenerator = (type, object) => {
+    if (type === 'keys') return Object.keys(object);
+    if (type === 'values') return Object.values(object);
+    if (type === 'entries') return Object.entries(object);
+};
 
 module.exports = { calculator, arrayGenerator };
